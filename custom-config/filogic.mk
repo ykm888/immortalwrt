b5dc467d4ebde473090cafbb34cfc6d85e49b468
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-02-05 21:57:13 +0800

    Update filogic.mk

--- custom-config/filogic.mk
+++ custom-config/filogic.mk
@@ -2,12 +2,11 @@ define Device/sl3000-emmc
   DEVICE_VENDOR := SL3000
   DEVICE_MODEL := Custom-1GB-Edition
   DEVICE_DTS := mt7981b-sl3000-emmc
-  # 必须添加路径索引，否则镜像合成器找不到 DTB 文件
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   SUPPORTED_DEVICES := sl,sl3000-emmc mediatek,mt7981b
   
   KERNEL_SIZE := 64M
-  IMAGE_SIZE := 1000M
+  IMAGE_SIZE := 1024M
   
   DEVICE_PACKAGES := \
 	kmod-mmc kmod-sdhci-mtk \
@@ -15,8 +14,7 @@ define Device/sl3000-emmc
 	kmod-usb3 kmod-usb-dwc3-mtk \
 	block-mount blkid lsblk parted
   
-  # 旗舰版必须使用 mtk-sdcard 逻辑生成带有分区表的镜像
   IMAGES := sysupgrade.bin
-  IMAGE/sysupgrade.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-rootfs | pad-rootfs | check-size | mtk-sdcard
+  IMAGE/sysupgrade.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-rootfs | pad-rootfs | check-size | append-metadata
 endef
 TARGET_DEVICES += sl3000-emmc
