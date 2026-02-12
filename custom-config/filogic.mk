8a851ba181435229018c056947f09f07db6b8eaf
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-02-13 00:50:40 +0800

    Update filogic.mk

--- custom-config/filogic.mk
+++ custom-config/filogic.mk
@@ -3,33 +3,17 @@ define Device/sl3000-emmc
   DEVICE_MODEL := 3000 (eMMC)
   DEVICE_ALT0_VENDOR := SL
   DEVICE_ALT0_MODEL := SL3000
-  DEVICE_ALT0_VARIANT := eMMC 512MB Rootfs
+  DEVICE_ALT0_VARIANT := eMMC 1024MB Rootfs
   DEVICE_DTS := mt7981b-sl3000-emmc
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := sl,sl3000-emmc mediatek,mt7981
+  SUPPORTED_DEVICES := sl,3000-emmc mediatek,mt7981
   KERNEL_SIZE := 131072k
-  BOARD_ROOTFS_PARTSIZE := 512
+  BOARD_ROOTFS_PARTSIZE := 1024
   KERNEL := kernel-bin | lzma | append-dtb
   KERNEL_INITRAMFS := kernel-bin | lzma | append-dtb
-  DEVICE_PACKAGES := \
-    kmod-mmc \
-    kmod-sdhci-mtk \
-    kmod-fs-f2fs \
-    f2fs-tools \
-    f2fsck \
-    parted \
-    lsblk \
-    blkid \
-    block-mount \
-    kmod-zram \
-    zram-swap
+  DEVICE_PACKAGES := kmod-mmc kmod-sdhci-mtk kmod-fs-f2fs f2fs-tools f2fsck parted lsblk blkid block-mount kmod-zram zram-swap
   IMAGES := sysupgrade.bin factory.bin
-  IMAGE/sysupgrade.bin := \
-    append-kernel | \
-    append-rootfs | \
-    append-metadata
-  IMAGE/factory.bin := \
-    append-kernel | \
-    append-rootfs
+  IMAGE/sysupgrade.bin := append-kernel | append-rootfs | append-metadata
+  IMAGE/factory.bin := append-kernel | append-rootfs
 endef
 TARGET_DEVICES += sl3000-emmc
