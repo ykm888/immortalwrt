85884ccdedb004196cd5bab1e875e734c6d14c73
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-02-19 10:37:28 +0800

    Update filogic.mk

--- custom-config/filogic.mk
+++ custom-config/filogic.mk
@@ -3,19 +3,21 @@ define Device/sl3000-emmc
   DEVICE_MODEL := 3000-eMMC
   DEVICE_DTS := mt7981b-3000-emmc
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := sl,3000-emmc mediatek,mt7981
+  SUPPORTED_DEVICES := sl,3000-emmc
   
-  # 物理偏移锁定
-  KERNEL_SIZE := 134217728
-  IMAGE_SIZE := 536870912
+  KERNEL_SIZE := 131072k
+  IMAGE_SIZE := 524288k
 
   KERNEL := kernel-bin | lzma | append-dtb
   DEVICE_PACKAGES := kmod-mmc kmod-mtk-sd kmod-fs-f2fs f2fs-tools f2fsck \
                     parted lsblk blkid block-mount kmod-zram zram-swap \
                     luci-app-diskman uboot-envtools
 
+  # 🔥 物理物理新增：定义产物收割逻辑
+  ARTIFACTS := fip.bin
+  ARTIFACT/fip.bin := mt7981-bl31-atf-install | mt7981-u-boot-install sl3000-emmc
+
   IMAGES := sysupgrade.bin
-  # 物理死锁：强制填充 128MB 对齐 DTS 分区表
   IMAGE/sysupgrade.bin := append-kernel | pad-to 128m | append-rootfs | check-size | append-metadata
 endef
 TARGET_DEVICES += sl3000-emmc
