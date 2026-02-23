738369edc916b6b96fbe2269d3e1e3fabce5bf61
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-02-23 13:02:53 +0800

    Update filogic.mk

--- custom-config/filogic.mk
+++ custom-config/filogic.mk
@@ -1,27 +1,52 @@
-define Device/sl3000-emmc
+DTS_DIR := $(DTS_DIR)/mediatek
+
+define Image/Prepare
+	# For UBI we want only one extra block
+	rm -f $(KDIR)/ubi_mark
+	echo -ne '\xde\xad\xc0\xde' > $(KDIR)/ubi_mark
+endef
+
+define Build/mt7981-bl2
+	cat $(STAGING_DIR_IMAGE)/mt7981-$1-bl2.img >> $@
+endef
+
+define Build/mt7981-bl31-uboot
+	cat $(STAGING_DIR_IMAGE)/mt7981_$1-u-boot.fip >> $@
+endef
+
+define Build/mt798x-gpt
+	cp $@ $@.tmp 2>/dev/null || true
+	ptgen -g -o $@.tmp -a 1 -l 1024 \
+		$(if $(findstring sdmmc,$1), \
+			-H \
+			-t 0x83	-N bl2		-r	-p 4079k@17k \
+		) \
+			-t 0x83	-N ubootenv	-r	-p 512k@4M \
+			-t 0x83	-N factory	-r	-p 2M@4608k \
+			-t 0xef	-N fip		-r	-p 4M@6656k \
+				-N recovery	-r	-p 32M@12M \
+		$(if $(findstring sdmmc,$1), \
+				-N install	-r	-p 20M@44M \
+			-t 0x2e -N production		-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@64M \
+		) \
+		$(if $(findstring emmc,$1), \
+			-t 0x2e -N production		-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@64M \
+		)
+	cat $@.tmp >> $@
+	rm $@.tmp
+endef
+
+define Device/sl_3000-emmc
   DEVICE_VENDOR := SL
   DEVICE_MODEL := 3000-eMMC
-  DEVICE_DTS := mt7981b-3000-emmc
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  DEVICE_DTS := mt7981b-sl-3000-emmc
+  DEVICE_DTS_DIR := ../dts
   SUPPORTED_DEVICES := sl,3000-emmc
-  
-  # 物理死锁：字节纯数字，防止 dd 报错
-  KERNEL_SIZE := 134217728
-  IMAGE_SIZE := 536870912
-
-  KERNEL := kernel-bin | lzma | append-dtb
-  DEVICE_PACKAGES := kmod-mmc kmod-mtk-sd kmod-fs-f2fs f2fs-tools f2fsck \
-                    parted lsblk blkid block-mount kmod-zram zram-swap \
-                    luci-app-diskman uboot-envtools
-
-  # 物理修复：移除导致 Missing Build 报错的 ARTIFACTS 段落
-  # 镜像生成逻辑
-  IMAGES := sysupgrade.bin
-  IMAGE/sysupgrade.bin := append-kernel | pad-to 134217728 | append-rootfs | check-size | append-metadata
+  DEVICE_PACKAGES := kmod-mmc kmod-mtk-sd kmod-fs-f2fs f2fs-tools e2fsprogs f2fsck mkf2fs kmod-zram zram-swap
+  IMAGE_SIZE := 512M
+  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += sl3000-emmc
-
-# 物理修复：针对 uboot-mediatek 报错点的 sed 修复指令
-# 注意：以下内容通常应在 package/boot/uboot-mediatek/Makefile 中执行
-# 修复了原先缺失的闭合斜杠 / 并在行尾插入
-# sed -i '/dtb- +=/ s/$$/ mt7981-sl3000-emmc.dtb/' $(PKG_BUILD_DIR)/arch/arm/dts/Makefile
+TARGET_DEVICES += sl_3000-emmc
