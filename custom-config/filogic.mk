c5339965a39f9a50d07238b0884a286e6b0b0f3e
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-02-23 19:34:23 +0800

    Update filogic.mk

--- custom-config/filogic.mk
+++ custom-config/filogic.mk
@@ -1,37 +1,22 @@
 DTS_DIR := $(DTS_DIR)/mediatek
 
 define Image/Prepare
-	# For UBI we want only one extra block
 	rm -f $(KDIR)/ubi_mark
-	echo -ne '\xde\xad\xc0\xde' > $(KDIR)/ubi_mark
-endef
-
-define Build/mt7981-bl2
-	cat $(STAGING_DIR_IMAGE)/mt7981-$1-bl2.img >> $@
+	echo -ne '\\xde\\xad\\xc0\\xde' > $(KDIR)/ubi_mark
 endef
 
 define Build/mt7981-bl31-uboot
-	cat $(STAGING_DIR_IMAGE)/mt7981_$1-u-boot.fip >> $@
+	cat $(STAGING_DIR_IMAGE)/*u-boot.fip >> $@
 endef
 
 define Build/mt798x-gpt
 	cp $@ $@.tmp 2>/dev/null || true
 	ptgen -g -o $@.tmp -a 1 -l 1024 \
-		$(if $(findstring sdmmc,$1), \
-			-H \
-			-t 0x83	-N bl2		-r	-p 4079k@17k \
-		) \
-			-t 0x83	-N ubootenv	-r	-p 512k@4M \
-			-t 0x83	-N factory	-r	-p 2M@4608k \
-			-t 0xef	-N fip		-r	-p 4M@6656k \
-				-N recovery	-r	-p 32M@12M \
-		$(if $(findstring sdmmc,$1), \
-				-N install	-r	-p 20M@44M \
-			-t 0x2e -N production		-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@64M \
-		) \
-		$(if $(findstring emmc,$1), \
-			-t 0x2e -N production		-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@64M \
-		)
+			-t 0x83 -N ubootenv -r -p 512k@4M \
+			-t 0x83 -N factory -r -p 2M@4608k \
+			-t 0xef -N fip -r -p 4M@6656k \
+				-N recovery -r -p 32M@12M \
+		$(if $(findstring emmc,$1), -t 0x2e -N production -p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@64M)
 	cat $@.tmp >> $@
 	rm $@.tmp
 endef
@@ -42,11 +27,9 @@ define Device/sl_3000-emmc
   DEVICE_DTS := mt7981b-sl-3000-emmc
   DEVICE_DTS_DIR := ../dts
   SUPPORTED_DEVICES := sl,3000-emmc
-  DEVICE_PACKAGES := kmod-mmc kmod-mtk-sd kmod-fs-f2fs f2fs-tools e2fsprogs f2fsck mkf2fs kmod-zram zram-swap
-  IMAGE_SIZE := 512M
-  KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
-  KERNEL_INITRAMFS := kernel-bin | lzma | \
-	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGES := sysupgrade.bin fip.bin
+  IMAGE/fip.bin := mt7981-bl31-uboot $(1)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := kmod-mmc kmod-mtk-sd kmod-fs-f2fs f2fs-tools e2fsprogs f2fsck mkf2fs kmod-zram zram-swap
 endef
 TARGET_DEVICES += sl_3000-emmc
