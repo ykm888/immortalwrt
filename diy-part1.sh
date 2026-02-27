d218e1a9224ac37421182975b311c857e9007d46
ykm888 <165190180+ykm888@users.noreply.github.com>
2026-02-27 20:19:12 +0800

    Delete diy-part1.sh

--- diy-part1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
